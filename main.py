139a64112073b98b35b53456595c60b82bee6fea
BaizeChen <42437720+574118090@users.noreply.github.com>
2023-10-20 13:30:26 +0800

    Update Model Frame

--- main.py
+++ main.py
@@ -3,51 +3,67 @@ import time
 from src.viewermodel import ViewerModel
 import threading
 
-play_model = ViewerModel(threshold=0.3, batch_size=16)
+play_model = ViewerModel(llm="gpt-4", threshold=0.3, batch_size=16, debug=False, decay_rate=0.7, removal_threshold=0.3)
 
 cap = cv2.VideoCapture(0)
 
 last_capture_time = 0
 
+class Relic:
+    def __init__(self):
+        self.answer = "test"
 
-def display_camera():
-    while True:
-        # Read frame from camera
-        ret, frame = cap.read()
-        if ret:
-            # Display the frame
-            cv2.imshow('frame', frame)
+    def display_camera(self):
+        # Create a named window with custom size
+        cv2.namedWindow('Relic', cv2.WINDOW_NORMAL)
 
-        # Exit if 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        while True:
+            # Read frame from camera
+            ret, frame = cap.read()
 
+            if ret:
+                # Split the frame into two parts
+                height, width, _ = frame.shape
+                split_line = height // 2
+                top_frame = frame[:split_line, :]
 
-def pipeline_task():
-    global last_capture_time
-    while True:
-        current_time = time.time()
-        ret, frame = cap.read()
-        if ret:
-            # Save the frame as an image
-            cv2.imwrite('./resources/input.jpg', frame)
-            # Process the image using the model
-            answer = play_model.pipeline('./resources/input.jpg')
+                # Resize frames to fit the big window
+                top_frame = cv2.resize(top_frame, (800, 300))
 
-            while len(answer) < 1:
-                # Retry processing if no answer is obtained
-                answer = play_model.pipeline('./resources/input.jpg')
+                # Create a new section with a gray background
+                relic_section = 100  # height of the new section
 
-            # Print the answer
-            print(answer)
+                # Display the frame in the big window
+                cv2.imshow('Relic', frame)
 
-            last_capture_time = current_time
+            # Exit if 'q' key is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
+    def pipeline_task(self):
+        global last_capture_time
+        while True:
+            current_time = time.time()
+            ret, frame = cap.read()
+            if ret:
+                # Save the frame as an image
+                cv2.imwrite('./resources/input.jpg', frame)
+                # Process the image using the model
+                self.answer = play_model.pipeline_str('./resources/input.jpg')
+
+                # Print the answer
+                print(self.answer)
+
+                last_capture_time = current_time
+
+            time.sleep(1)
 
 def run_threads():
+    relic = Relic()
+
     # Create and start threads for camera display and pipeline tasks
-    display_thread = threading.Thread(target=display_camera)
-    pipeline_thread = threading.Thread(target=pipeline_task)
+    display_thread = threading.Thread(target=relic.display_camera)
+    pipeline_thread = threading.Thread(target=relic.pipeline_task)
 
     display_thread.start()
     pipeline_thread.start()
@@ -55,7 +71,6 @@ def run_threads():
     # Wait for both threads to finish
     display_thread.join()
     pipeline_thread.join()
-
 
 if __name__ == "__main__":
     # Run the threads
--- src/viewermodel.py
+++ src/viewermodel.py
@@ -1,64 +1,84 @@
 import json
 import cProfile
-from deepdanbooru_onnx import DeepDanbooru
-import os
 import openai
+import string
+from deepdanbooru_onnx import DeepDanbooru
 
 
 class ViewerModel:
-    def __init__(self, threshold=0.5, batch_size=1):
+    def __init__(self,
+                 threshold=0.5,
+                 batch_size=1,
+                 debug=False,
+                 decay_rate=0.5,
+                 removal_threshold=0.1,
+                 llm="gpt-3.5-turbo"
+                 ):
+
+        # Set OpenAI API configuration
         openai.api_base = 'https://api.closeai-proxy.xyz/v1'
         openai.api_key = 'sk-WYkFRLZwB9vBzh5RLusi7VuA1HtSt496Wvc5PyVO1SD2ApSa'
 
-        # Initialize DeepDanbooru class with given parameters
-        self.deep_danbooru = DeepDanbooru(threshold=threshold, batch_size=batch_size)
+        # Initialize DeepDanbooru with given parameters
+        self.deep_danbooru = DeepDanbooru(mode='auto', threshold=threshold, batch_size=batch_size)
 
-        # Keep track of the previous frame's analyzed label and confidence
+        # Store previous frame's analyzed label and confidence
         self.pre_str = ""
+        self.str_debug = debug
 
-    def deep_danbooru_str(self, image_path):
+        # Initialize keywords dictionary
+        self.keywords = {}
+
+        # Add decay rate and removal threshold as class members
+        self.decay_rate = decay_rate
+        self.removal_threshold = removal_threshold
+
+        self.llm = llm
+
+    def update_keywords(self):
+        # Update the weights of the keywords by subtracting the decay_rate
+        for key in list(self.keywords.keys()):
+            self.keywords[key] *= self.decay_rate
+            if self.keywords[key] < self.removal_threshold:
+                del self.keywords[key]
+        if self.str_debug:
+            print("current keywords:", self.keywords)
+
+    def deep_danbooru(self, image_path):
         # Helper function to get DeepDanbooru predictions on a single image
         js = self.deep_danbooru(image_path)
-        return str(js)
+        for item, weight in js.items():
+            if item in self.keywords:
+                self.keywords[item] += weight
+                self.keywords[item] = min(1.0, self.keywords[item])
+            else:
+                self.keywords[item] = weight
+        return js
 
-    def message_constructor(self, str1):
-        # Helper function to construct the prompt to send to GPT-3 API
-        message = "There is a video with the following label and confidence for the previous frame: "
-        message += self.pre_str
-        message += ". And for the current frame, the following label and confidence are: "
-        message += str1
-        message += ". These two frames are consecutive. Please guess the scene in which this " \
-                   "image is located based on this information."
+    def message_constructor(self, current_keywords):
+        # Convert keywords dictionary into a readable string for both self.keywords and current_keywords
+        self_keywords_str = ', '.join([f"{key}: {value}" for key, value in self.keywords.items()])
+        current_keywords_str = ', '.join([f"{key}: {value}" for key, value in current_keywords.items()])
+
+        # Construct the prompt for GPT API
+        message = f"The previous frame was described as: '{self.pre_str}'. " \
+                  f"Based on the previous keywords: {self_keywords_str} and current keywords: {current_keywords_str}, " \
+                  "if you infer that the current scene is similar to the previous frame, " \
+                  "return the previous description without any changes. " \
+                  "Otherwise, provide a new description for the current frame."
+
+        if self.str_debug:
+            print("message:", message)
         return message
 
-    def pipeline(self, image_path):
-        # Main pipeline function that takes an image path as input and outputs a JSON object
-        str1 = self.deep_danbooru_str(image_path)
-
-        # Save current label and confidence to use for next frame
-        self.pre_str = str1
-
-        # Use GPT-3 API to generate the scene description based on previous and current frame visual data
-        str2 = self.gpt(str1)
-
-        # Transform the GPT-3 output text into a JSON object
-        return self.json_transformer(str2)
-
-    def gpt(self, str1):
-        # Helper function that sends a prompt to the GPT-3 API and returns its response
-        message = self.message_constructor(str1)
+    def gpt_output_str(self, current_keywords):
+        # Helper function that sends a prompt to the GPT API and returns its response
+        message = self.message_constructor(current_keywords)
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model=self.llm,
             messages=[
                 {"role": "system",
-                 "content": "Output a json file in the following format:"
-                            "{"
-                            "indoor or outdoor: str, "
-                            "weather: str, scene: str, "
-                            "objects: [str], color: str, "
-                            "feeling: str"
-                            "people counts: int"
-                            "}"
+                 "content": "Based on the provided information, describe the scene using a phrase or sentence of no more than 5 words. Do not include any reference to humans and animals, human-like figures, or any form or implication of a person's presence. Also, avoid using any adjectives."
                  },
                 {"role": "user",
                  "content": message
@@ -66,27 +86,26 @@ class ViewerModel:
             ]
         )
         output = response["choices"][0]["message"]["content"]
-
+        self.pre_str = output
         return output
 
-    def json_transformer(self, gpt_output):
-        # Helper function that transforms GPT-3 output text into a JSON object
-        try:
-            json_dict = json.loads(gpt_output)
-            if isinstance(json_dict, dict):
-                return json_dict
-            else:
-                return {}
-        except json.JSONDecodeError:
-            print("Error!")
-            return {}
+    def remove_punctuation(self, s):
+        # 使用translate方法移除字符串中的所有标点符号
+        return s.translate(str.maketrans('', '', string.punctuation))
+
+    def pipeline_str(self, image_path):
+        current_keywords = self.deep_danbooru(image_path)
+        output_desc = self.gpt_output_str(current_keywords)
+        output_desc = self.remove_punctuation(output_desc)  # 调用remove_punctuation方法
+        self.update_keywords()
+        return output_desc
 
     def debug(self, image_path):
-        # Debug function that profile the pipeline execution time
+        # Debug function to profile the pipeline execution time
         cProfile.runctx('self.pipeline(image_path)', globals(), locals(), sort='cumulative')
 
 
 if __name__ == '__main__':
     # Instantiate the model and call the pipeline function with an example image
     model = ViewerModel(threshold=0.2)
-    print(model.pipeline("../resources/input.jpg"))
+    print(model.pipeline_str("../resources/input.jpg"))
